569d46744b16cb5ba8d27c14fcaf7ba756be076b
Lars Næsbye Christensen <lars@naesbye.dk>
2023-02-21 10:55:16 +0100

    switch to using pathlib

--- src/ab/data.py
+++ src/ab/data.py
@@ -2,29 +2,32 @@
 Module for downloading source data
 
 """
-import os
+
 import requests
 from ftplib import FTP, FTP_TLS
+from pathlib import Path
 
 
 def download_http(domain: str, path: str, localpath: str) -> None:
     """Handles HTTP and HTTPS downloads without credentials.
     Allow following redirects for simplicity."""
     r = requests.get(domain + path, allow_redirects=True)
-    open(localpath + os.path.basename(path), "wb").write(r.content)
+    path = Path(localpath)
+    open(localpath + path.name, "wb").write(r.content)
 
 
-def download_ftp(scheme: str, domain: str, path: str, localpath: str) -> None:
-    """Handles FTP (insecure) and FTPS downloads.
-    FTP should NOT be used except with 'anonymous' as username and password."""
+def download_ftp(scheme: str, domain: str, remotepath: str, localpath: str) -> None:
+    """Handles FTP (insecure) and FTPS downloads using anonymous user."""
+    localfilepath = Path(localpath)
+    remotefilepath = Path(remotepath)
     if scheme == "ftps":
         ftp = FTP_TLS(domain)  # connect to host, default port
     else:
         ftp = FTP(domain)
     ftp.login()  # user and pass is anonymous
-    ftp.cwd(os.path.dirname(path))  # change into the specified directory
-    with open(localpath + os.path.basename(path), "wb") as fp:
-        ftp.retrbinary("RETR %s" % os.path.basename(path), fp.write)
+    ftp.cwd(str(remotefilepath.parent))  # change into the path's parent
+    with open(localfilepath, "wb") as fp:
+        ftp.retrbinary("RETR %s" % remotefilepath.name, fp.write)
     ftp.quit()
 
 
