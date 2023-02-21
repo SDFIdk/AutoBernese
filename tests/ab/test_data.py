031885d9c77be2d11401bcf1e3cd6a2ac8cff691
Kristian Evers <kristianevers@gmail.com>
2023-02-21 21:49:56 +0100

    Update tests/ab/test_data.py

--- tests/ab/test_data.py
+++ tests/ab/test_data.py
@@ -13,7 +13,7 @@ from ab.data import calc_gps_week
     [
         # Date, GPS week
         (dt.date(1980, 1, 6), 0),
-        (dt.date(1985, 7, 25), 288),
+        (dt.date(1985, 7, 25), 289),
         (dt.date(1994, 2, 8), 735),
         (dt.date(2015, 10, 14), 1866),
         (dt.date(2023, 2, 21), 2250),
