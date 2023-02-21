2763a859d7f80f140a6b7878a585127277bd9aa2
Lars Næsbye Christensen <lars@naesbye.dk>
2023-02-21 13:31:36 +0100

    Add method for calculating GPS week number for a given date.

--- src/ab/data.py
+++ src/ab/data.py
@@ -2,6 +2,15 @@
 Module for downloading source data
 
 """
+from datetime import date, timedelta
+
+
+def calc_gps_week(tocalc: date) -> int:
+    """Calculates the GPS week number for a given date"""
+    gps_epoch = date(1980, 1, 6)  # first GPS week
+    epoch_monday = gps_epoch - timedelta(gps_epoch.weekday())
+    today_monday = tocalc - timedelta(tocalc.weekday())
+    return int((today_monday - epoch_monday).days / 7)
 
 
 def download_sources() -> None:
