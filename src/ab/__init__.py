e4a94191d38a4ab97bae39cc869eafe68656d645
Lars Næsbye Christensen <lars@naesbye.dk>
2023-02-28 15:54:15 +0100

    Add expansion of the YAML to accommodate logging configuration

--- src/ab/__init__.py
+++ src/ab/__init__.py
@@ -5,6 +5,9 @@ AutoBernese
 from importlib import metadata
 
 import ab
-
+from ab import configuration
 
 __version__ = metadata.version(ab.__name__)
+
+log_yaml = configuration.load()
+# Here should follow parsing of the entire configuration
