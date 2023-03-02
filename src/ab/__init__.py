11d8306810a4217f2d0bd6febeffa6adeba64ea0
Lars Næsbye Christensen <lars@naesbye.dk>
2023-03-02 12:23:47 +0100

    Set logging path to the parent of the environment variable 'C', defaulting to . if not present

--- src/ab/__init__.py
+++ src/ab/__init__.py
@@ -4,11 +4,18 @@ AutoBernese
 """
 from importlib import metadata
 import logging
+import pathlib
 
 import ab
-from ab import configuration
+import configuration
 
 __version__ = metadata.version(ab.__name__)
 
 config = configuration.load()
-logging.basicConfig(**config.get("logging"))
+
+# Set up logging
+log_kwargs = config.get("logging")
+bsw_path = config.get("bsw_path")  # Get the path to the Bernese installation directory
+env_path = pathlib.Path(bsw_path).parent
+log_kwargs["filename"] = env_path / log_kwargs.get("filename", "autobernese.log")
+logging.basicConfig(**log_kwargs)  # Set up basic logging using the loaded configuration
