1dda8577eca8b1bab61f305e8b03c9b5c43e8db4
Joachim Mortensen <xidus@users.noreply.github.com>
2023-03-14 12:55:53 +0100

    Re-organise for clarity

--- src/ab/__init__.py
+++ src/ab/__init__.py
@@ -9,13 +9,20 @@ import pathlib
 import ab
 from ab import configuration
 
+
 __version__ = metadata.version(ab.__name__)
 
 config = configuration.load()
 
-# Set up logging
-log_kwargs = config.get("logging")
-bsw_path = config.get("bsw_path")  # Get the path to the Bernese installation directory
+# Get the path to the Bernese installation directory
+bsw_path = config.get("bsw_path", ".")
+
+# Note that a the path `.` makes the `.parent` property return itself.
 env_path = pathlib.Path(bsw_path).parent
-log_kwargs["filename"] = env_path / log_kwargs.get("filename", "autobernese.log")
-logging.basicConfig(**log_kwargs)  # Set up basic logging using the loaded configuration
+
+# Set filename path to the parent directory of the given installation path.
+log_settings = config.get("logging")
+log_settings["filename"] = env_path / log_settings.get("filename", "autobernese.log")
+
+# Set global log settings for the root logger
+logging.basicConfig(**log_settings)
