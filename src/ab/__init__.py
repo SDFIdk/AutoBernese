0283b776a3ccc2d797292f697e577c04c5505d9a
Joachim Mortensen <xidus@users.noreply.github.com>
2023-04-24 14:51:19 +0200

    Re-organise configuration file.

--- src/ab/__init__.py
+++ src/ab/__init__.py
@@ -10,4 +10,7 @@ from ab import configuration
 
 
 __version__ = metadata.version(ab.__name__)
-logging.basicConfig(**configuration.load().get("logging"))
+env = configuration.load().get("environment")
+env.get("ab_root").mkdir(exist_ok=True)
+env.get("logging").get("filename").touch()
+logging.basicConfig(**env.get("logging"))
