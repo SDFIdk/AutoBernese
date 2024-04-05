c24c4fa21c53272f645a681991280f7c22e0e62d
Joachim Mortensen <xidus@users.noreply.github.com>
2024-04-05 15:28:12 +0200

    Remove refactored script.

--- src/ab/__init__.py
+++ src/ab/__init__.py
@@ -3,19 +3,8 @@ AutoBernese
 
 """
 from importlib import metadata
-import getpass
-import logging
 
 import ab
-from ab import configuration
 
 
 __version__ = metadata.version(ab.__name__)
-runtime = configuration.load().get("runtime")
-runtime.get("ab").mkdir(exist_ok=True)
-log_kw = runtime.get("logging")
-log_kw.get("filename").touch()
-replacements = dict(
-    format=log_kw.get("format").format(user=getpass.getuser()),
-)
-logging.basicConfig(**{**log_kw, **replacements})
