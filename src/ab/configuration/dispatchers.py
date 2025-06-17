f21f14c0ba1bf0570f2abb74374f930c1529bfb3
Joachim Mortensen <xidus@users.noreply.github.com>
2025-06-17 15:12:05 +0200

    maint: Add type for expected signature for ab's gzip function.

--- src/ab/configuration/dispatchers.py
+++ src/ab/configuration/dispatchers.py
@@ -1,17 +1,25 @@
 """
-Dispatch functions are functions that restructure or otherwise pre-process given arguments
-to make them work for a specific API-level function used by the TaskDefinition.
+Dispatch functions are functions that restructure or otherwise pre-process given
+arguments to make them work for a specific API-level function used by the
+TaskDefinition.
+
+Return type for dispatch function must be an Iterable match signature of
+API-level function.
 
 """
 
 from typing import Any
+from collections.abc import Iterable
 
 from ab.parameters import ArgumentsType
 from ab.paths import resolve_wildcards
 from ab.data import compress
 
 
-def gzip_dispatch(arguments: ArgumentsType) -> list[dict[str, Any]]:
+type GZipCompressArgumentType = dict[str, Any]
+
+
+def gzip_dispatch(arguments: ArgumentsType) -> Iterable[GZipCompressArgumentType]:
     key = "fname"
     filenames = resolve_wildcards(arguments[key])
     return [{**arguments, **{key: fname}} for fname in filenames]
